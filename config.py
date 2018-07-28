f7563818d130b8d10f894c04f609487c7a5c041a
TobaisY <38781069+TobaisY@users.noreply.github.com>
2018-07-29 02:14:18 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from datetime import timedelta
 DIALECT = "mysql"
 DRIVER = "pymysql"
 USERNAME = "root"
-PASSWORD = 'qwe45623'
+PASSWORD = ''
 HOST = '127.0.0.1'
 PORT = '3306'
-DATABASE = 'TobiasWeb'
+DATABASE = ''
 SQLALCHEMY_DATABASE_URI = "{}+{}://{}:{}@{}:{}/{}?charset=utf8".format(DIALECT, DRIVER, USERNAME, PASSWORD, HOST, PORT,
 																	   DATABASE)
 SQLALCHEMY_TRACK_MODIFICATIONS = True
